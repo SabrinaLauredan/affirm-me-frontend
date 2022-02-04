d87f2a7130b93a1188e7c7d15a408dc39883cf8c
Sabrina Lauredan <slauredan@gmail.com>
2022-02-03 19:15:28 -0500

    goalcard and goallist components created and displaying on react

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
+import * as React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home';
 import About from './Pages/About';
 import ErrorPage from './Pages/ErrorPage';
+import { GoalList } from './Components/GoalList';
 
 function App() {
 	return (
 	<Router>
 		<nav> 
-			{/* //we can use React Link component in place of a tags */}
 			<Link to="/"> Home </Link>
 			<Link to="/about"> About </Link>
 		</nav>
-		{/* // ^ displays across all pages, the only thing that changes is everything below  */}
 		<Routes>
-			{/* a route is declared using a Route component */}
 			<Route path="/" element={<Home />} /> 
-      {/* "/" is the homepage  */}
 			<Route path="/about" element={<About />} />
 			<Route path="/*" element={<ErrorPage />} />
-      {/* must be the last route, "/*" says every /example, besides the one defined above */}
 		</Routes>
+		< GoalList />
 		<div> footer </div> 
 	</Router>
 	);
 
 }
-
-export default App;
+export default App; 
---
+++ src/Components/GoalList.tsx
@@ -0,0 +1,10 @@
+import GoalCard from './GoalCard';
+
+
+const cards = [1,2,3,4,5,6];
+
+export function GoalList (){
+return (
+    <GoalCard />
+)
+}
