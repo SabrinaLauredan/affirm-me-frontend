b1e171c04b6f52bf463bfb25b1067ff6f7c41677
Sabrina Lauredan <slauredan@gmail.com>
2022-02-09 15:30:20 -0500

    updates to GoalCardView: addition of back button and calendar

--- src/Components/GoalCardView.tsx
+++ src/Components/GoalCardView.tsx
@@ -1,17 +1,16 @@
-import * as React from "react";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar'; 
+import 'react-calendar/dist/Calendar.css';
 // import { getGoals} from "./api";
 
 const GoalCardView: React.FC<{goalDetail:any, setGoalDetail:Function}> = ({goalDetail, setGoalDetail}) => {
-
-    // when goal is clicked, goal view should expand and 
-    // show the goalview component on click 
-    // takes 
+    const [value, onChange] = useState(new Date());
 
     return  (
         <div className="goal-view">
             <h2>Goal View</h2>
             <button onClick = {() => setGoalDetail(null)}>
-                close
+                back to home
             </button>
             <div className="goal-view-blocks">
                 <div>
@@ -23,23 +22,12 @@ const GoalCardView: React.FC<{goalDetail:any, setGoalDetail:Function}> = ({goalD
                     </div>
                 </div>
             </div>
-
+            <div>
+            <h1>CALENDAR DISPLAY</h1>
+            <Calendar onChange={onChange} value ={value} />
+            </div>
         </div>
     )
-
-
-
-    // const { id } = useParams();
-    // const post = getGoals(id);
-
-
-
-
-    // return (
-    //     <div>
-    //         <h1>{post.title}</h1>
-    //         <p>{post.content}</p>
-    //return "goal card view"
 };
 
 export default GoalCardView;
