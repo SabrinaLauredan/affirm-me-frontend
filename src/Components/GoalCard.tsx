d59b92a3d5879ac9771943af01808987cfa38b90
Sabrina Lauredan <slauredan@gmail.com>
2022-02-10 20:12:22 -0500

    a merge

--- src/Components/GoalCard.tsx
+++ src/Components/GoalCard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Button, Card, Row, Col } from 'react-bootstrap';
+import { Button, Card, Row, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link } from 'react-router-dom'
 
 
 
@@ -8,22 +9,25 @@ const GoalCard: React.FC<{ goal: any, setGoalDetail: Function }> = ({ goal, setG
     return (
         <Row xs={1} md={2} className="g-4">
             {/* {Array.from({ length: 4 }).map((_, idx) => ( */}
-                <Col>
-                    <Card className="text-center" border="info" style={{ width: '18rem' }}>
-                        <Card.Header> Goal #{goal.id}</Card.Header>
-                        <Card.Body>
-                            <Card.Title as="h5">
-                                {goal.title}
-                            </Card.Title>
-                            <Card.Text>
-                                {goal.description}
-                            </Card.Text>
-                            <Button variant="primary" onClick={() => { setGoalDetail(goal) }}>Card Details</Button>
-                            <Button variant="primary">Delete Goal</Button>
-                        </Card.Body>
-                        <Card.Footer className="text-muted">Created on: {goal.createdAt}</Card.Footer>
-                    </Card>
-                </Col>
+            <Col>
+                <Card className="text-center" border="info" style={{ width: '18rem' }}>
+                    <Card.Header> Goal #{goal.id}</Card.Header>
+                    <Card.Body>
+                        <Card.Title as="h5">
+                            {goal.title}
+                        </Card.Title>``
+                        <Card.Text>
+                            {goal.description}
+                        </Card.Text>
+                        <Button variant="primary" onClick={() => { setGoalDetail(goal) }}>Card Details</Button>
+                        <Button variant="primary">Delete Goal</Button>
+                        <button>
+                            <Link to={`/goalform?id=${goal.id}`}  className="nav-link">Edit Goal</Link>
+                        </button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">Created on: {goal.createdAt}</Card.Footer>
+                </Card>
+            </Col>
             {/* ))} */}
         </Row>
     );
