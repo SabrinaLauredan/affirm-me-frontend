f46f9adc674e9a13161f73ba514296fa3315d213
Aisha <63489725+AishaA-cpu@users.noreply.github.com>
2022-02-13 13:33:33 -0600

    changes export to default

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { GoalsList } from "../components/GoalsList";
+import GoalsList from "../components/GoalsList";
 
 const Home = () => {
     const navigate = useNavigate()
