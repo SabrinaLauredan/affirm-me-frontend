50c96c363b70c237f50fa4eba48ee92f0af01eb4
Asli Athman <asliathman@gmail.com>
2022-02-18 04:36:55 -0500

    container to home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import GoalsList from "../Components/GoalsList";
-import { Button } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 const Home = () => {
     const navigate = useNavigate()
 
     return (
         <div>
-            <Button
-                onClick={() => {
-                    navigate('/goalform');
-                }}
-            >
-                +
-            </Button>
-            {/* <h1> affirm.me (THIS IS THE HOME PAGE)</h1> */}
-            < GoalsList />
+            <Container>
+                <Button
+                    onClick={() => {
+                        navigate('/goalform');
+                    }}
+                >
+                    +
+                </Button>
+                {/* <h1> affirm.me (THIS IS THE HOME PAGE)</h1> */}
+                < GoalsList />
+
+            </Container>
+            
         </div>
     )
 }
