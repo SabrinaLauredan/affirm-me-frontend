c39ba1aa3ba9f161e3affe597c218951322a630f
Asli Athman <asliathman@gmail.com>
2022-02-18 06:50:40 -0500

    added arda naimation to affirmationmessage

--- src/Components/AffirmationMessage.tsx
+++ src/Components/AffirmationMessage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Affirmation from "./Affirmation";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Stack, CloseButton, Figure} from "react-bootstrap";
+import gif from '../assets/affirmation.gif';
 
 //prop interface 
 //response to check-in data = string
@@ -20,19 +21,22 @@ const AffirmationMessage = () => {
     return (
         <div>
             <CloseButton aria-label="Hide" onClick={goBackHome} />
-            <div className="animation">{animation}
-            <Figure>
+            <div className="animation">
+                <img width={250} height={200} src={gif} alt="affirmationanimation" />
+                <h3 className="message">
+                    <Affirmation response={answer} />
+                </h3>
+            {/* <Figure>
                 <Figure.Image
                     width={171}
                     height={180}
-                    alt="171x180"
-                    src="holder.js/171x180"
+                    alt="affirmation gif"
+                    src=""
                 />
                 <Figure.Caption>
                     <Affirmation response={answer} />
-                    {/* <h3>message test</h3> */}
                 </Figure.Caption>
-            </Figure>
+            </Figure> */}
             </div> 
         </div>
     );
