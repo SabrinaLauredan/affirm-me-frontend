9710dd99cf8cfc8f2a699fd798da5283f5ac5391
Sabrina Lauredan <slauredan@gmail.com>
2022-02-18 17:16:49 -0500

    formatted about page

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,45 +1,51 @@
 import React from "react";
-import { Accordion } from "react-bootstrap";
-// import dancing from '../assets/dancing.png';
+import { Accordion, Stack } from "react-bootstrap";
+import Image from 'react-bootstrap/Image'
 import mascot from '../assets/mascot.jpg'
 
-// add snippet for setting up typescript component w basic text message
-
-
-
 const About = () => {
-
     return (
         <div>
-            {/* <h1>about</h1> */}
-            <br />
-            <img className="img-fluid" src={mascot} alt="dancing-img" />
-            <Accordion defaultActiveKey="0" flush>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>team members</Accordion.Header>
-                    <Accordion.Body>
-                        Aisha, Asli, and Sabrina
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>inspiration</Accordion.Header>
-                    <Accordion.Body>
-                            The idea is based partly on this reddit page for people who are trying to
-                        moderate or completely stop drinking called 'I Will Not Drink With You Today (IWNDWYT)' where strangers post about their
-                        journey into sobriety. A lot of times people post when trying again after falling off the wagon and get messages of
-                        encouragement from strangers in a similar positions as them (they end these messages with IWNDWYT, to say 'hey, I will
-                        not drink with you today' we are in this together!!!) Everything is easier when we do it together, which is what I hope
-                        this app will encourage people to do: challenge and support each other through successes and failures.
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>github</Accordion.Header>
-                    <Accordion.Body>
-                        <li>frontend</li>
-                        <li>backend</li>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+            <Stack direction="horizontal" gap={3}>
+                <div className="bg-light border">
+                    <div style={{ width: 500, padding: 30 }}>
+                        <Image fluid rounded src={mascot} alt="dancing-img">
+                        </Image>
+                    </div>
+                </div>
+                <div className="yr" />
+                <div className="bg-light border">
+                    <Accordion flush>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>created by</Accordion.Header>
+                            <Accordion.Body>
+                                aisha arigbabu, asli athman, and sabrina lauredan
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>inspiration</Accordion.Header>
+                            <Accordion.Body>
+                                The idea is based partly on this reddit page for people who are trying to
+                                moderate or completely stop drinking called 'I Will Not Drink With You Today (IWNDWYT)' where strangers post about their
+                                journey into sobriety. A lot of times people post when trying again after falling off the wagon and get messages of
+                                encouragement from strangers in a similar positions as them (they end these messages with IWNDWYT, to say 'hey, I will
+                                not drink with you today' we are in this together!!!) Everything is easier when we do it together, which is what I hope
+                                this app will encourage people to do: challenge and support each other through successes and failures.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>source code</Accordion.Header>
+                            <Accordion.Body>
+                                <li>frontend</li>
+                                <li>backend</li>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </div>
+                {/* <div className="bg-light border">
+                    <h1>about</h1>
+                </div> */}
+            </Stack>
         </div>
     )
 }
